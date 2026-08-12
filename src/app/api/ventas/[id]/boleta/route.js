2eb2b4679ace7a153453c199b6042f270b81a6cc
Leonardo Vasquez <devnRoot@gmail.com>
2026-08-12 18:46:35 -0500

    cambiando el numero correlativo/prueba

--- src/app/api/ventas/[id]/boleta/route.js
+++ src/app/api/ventas/[id]/boleta/route.js
@@ -37,7 +37,7 @@ export async function POST(req, { params }) {
 
    // CORRELATIVO MANUAL POR AHOR
     const serie = "B001";
-    const numero = 7;
+    const numero = 9;
 
     const venta = await prisma.venta.findUnique({
       where: {
